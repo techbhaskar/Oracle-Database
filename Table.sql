c72303cc2f178a74bddd270b9920f6ba5e09b148
Arani Bhaskararao <bhaskara.ee@gmail.com>
2019-04-28 02:41:05 +0530

    Update Table.sql

--- Table.sql
+++ Table.sql
@@ -7,3 +7,22 @@ create table test_department(
   location      varchar2(50),   
   constraint pk_departments primary key (deptno)   
 )
+
+
+create table test_employee (  
+  empno             number,  
+  name              varchar2(50) not null,  
+  job               varchar2(50),  
+  manager           number,  
+  hiredate          date,  
+  salary            number(7,2),  
+  commission        number(7,2),  
+  deptno           number,  
+  constraint pk_employees primary key (empno),  
+  constraint fk_employees_deptno foreign key (deptno) 
+      references test_department (deptno)  
+);
+
+/** 
+* Note: Foreign keys must reference primary keys, so to create a "child" table the "parent" table must have a primary key for the foreign key to reference.
+*/
