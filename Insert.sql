d686fdfb23550af3a7705efe74cbb7efd925553b
Arani Bhaskararao <bhaskara.ee@gmail.com>
2019-05-05 23:49:48 +0530

    Update Insert.sql

--- Insert.sql
+++ Insert.sql
@@ -9,3 +9,15 @@ INSERT INTO categories (category_id, category_name) VALUES (103, 'Demo 3');
 INSERT INTO  categories (category_id, category_name) SELECT id, name FROM test_customers;
 
 INSERT INTO  categories (category_id, category_name) SELECT id, name FROM test_customers where id>100;
+
+INSERT ALL
+  INTO mytable (column1, column2, column_n) VALUES (expr1, expr2, expr_n)
+  INTO mytable (column1, column2, column_n) VALUES (expr1, expr2, expr_n)
+  INTO mytable (column1, column2, column_n) VALUES (expr1, expr2, expr_n)
+SELECT * FROM dual;
+
+INSERT ALL
+  INTO suppliers (supplier_id, supplier_name) VALUES (1000, 'IBM')
+  INTO suppliers (supplier_id, supplier_name) VALUES (2000, 'Microsoft')
+  INTO customers (customer_id, customer_name, city) VALUES (999999, 'Anderson Construction', 'New York')
+SELECT * FROM dual;
